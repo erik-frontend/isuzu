8fa9e5a053ca6d50cfe1522f338bbc659f8c3e06
Erik <130284367+erik-frontend@users.noreply.github.com>
2026-05-11 20:29:11 +0200

    commit

---
+++ js/catalog.js
@@ -0,0 +1,13 @@
+$('.filter-group').click(function () {
+
+    $(this).toggleClass('active');
+
+});
+
+
+
+$('.catalog-subcategories__link').click(function () {
+
+    $(this).toggleClass('active');
+
+});
