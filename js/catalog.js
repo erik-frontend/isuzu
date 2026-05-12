f4fccffc43d1a8416ac4d5dc0b091315cf87b97b
Erik <130284367+erik-frontend@users.noreply.github.com>
2026-05-12 14:16:10 +0200

    commit
    added js active class toogles

--- js/catalog.js
+++ js/catalog.js
@@ -1,13 +1,27 @@
-$('.filter-group').click(function () {
+$('.category-list__item').click(function () {
+    $(".category-list__item").removeClass("active")
+    $(this).addClass('active');
 
-    $(this).toggleClass('active');
+});
+
+$('.filter-group__header').click(function () {
+
+    $(this).parent().toggleClass('active');
 
 });
 
 
 
-$('.catalog-subcategories__link').click(function () {
-
-    $(this).toggleClass('active');
-
+$('.catalog-subcategories__link').click(function (e) {
+    e.preventDefault();
+    if ($(this).hasClass("active")) {
+        $(this).removeClass("active")
+    } else {
+        $(".catalog-subcategories__link").removeClass("active")
+        $(this).addClass('active');
+    }
 });
+
+
+
+
