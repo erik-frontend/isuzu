253e9a1ce8c8ac8582c1ea640ba000c24945349e
Erik <130284367+erik-frontend@users.noreply.github.com>
2026-05-12 15:54:51 +0200

    added rating

--- js/catalog.js
+++ js/catalog.js
@@ -24,4 +24,32 @@ $('.catalog-subcategories__link').click(function (e) {
 
 
 
+// const ratings = document.querySelectorAll(".rating")
+
+// ratings.forEach((rating) => {
+//     const value = Number(rating.dataset.rating)
+
+//     for(let i = 1; i <= 5; i++){
+//         const star = document.createElement('span')
+//         star.classList.add("rating__star")
+
+//         if(i <= value) {
+//             star.classList.add("active")
+//         }
+
+//         rating.append(star);
+//     }
+// })
+
+
+const $stars = $(".star");
+
+$stars.on('click', function () {
+    let index = $(this).index();
+    $stars.removeClass('active')
+        .slice(0, index + 1)
+        .addClass('active')
+})
+
+
 
